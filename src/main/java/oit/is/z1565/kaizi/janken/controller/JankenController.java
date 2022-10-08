59d87931e69b1262a31249a4d55b8896855b3a0e
YutoMina <e1b20096@st.oit.ac.jp>
2022-10-08 22:43:05 +0900

    ユーザー名の表示機能の実装

---
+++ src/main/java/oit/is/z1565/kaizi/janken/controller/JankenController.java
@@ -0,0 +1,24 @@
+package oit.is.z1565.kaizi.janken.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class JankenController {
+
+  @GetMapping("/janken")
+  public String janken() {
+    return "janken.html";
+  }
+
+  @PostMapping("/janken")
+  public String janken(@RequestParam String username, ModelMap model) {
+    model.addAttribute("userName", username);
+    return "janken.html";
+  }
+
+}
