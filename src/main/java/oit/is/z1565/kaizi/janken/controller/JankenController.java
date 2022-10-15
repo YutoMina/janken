e2948f395837131d2876772fbef2817d220fd9d3
YutoMina <e1b20096@st.oit.ac.jp>
2022-10-16 02:23:04 +0900

    オブジェクトの共有

--- src/main/java/oit/is/z1565/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1565/kaizi/janken/controller/JankenController.java
@@ -1,31 +1,49 @@
 package oit.is.z1565.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.z1565.kaizi.janken.model.Entry;
 
 @Controller
 public class JankenController {
+  @Autowired
+  private Entry room;
 
   @GetMapping("/janken")
-  public String janken() {
+  public String janken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+    model.addAttribute("userName", loginUser);
     return "janken.html";
   }
 
-  @GetMapping("/janken.html")
-  public String janken2() {
-    return "janken.html";
-  }
+  /*
+   * @GetMapping("/janken.html")
+   * public String janken2(Principal prin, ModelMap model) {
+   * String loginUser = prin.getName();
+   * this.room.addUser(loginUser);
+   * model.addAttribute("room", this.room);
+   * return "janken.html";
+   * }
+   */
 
-  @PostMapping("/janken")
-  public String jankenNamed(@RequestParam String username, ModelMap model) {
-
-    model.addAttribute("userName", username);
-    return "janken.html";
-  }
+  /*
+   * @PostMapping("/janken")
+   * public String jankenNamed(@RequestParam String username, ModelMap model) {
+   *
+   * model.addAttribute("userName", username);
+   * return "janken.html";
+   * }
+   */
 
   @GetMapping("janken/{param}")
   public String jankenFight(@PathVariable Integer param, ModelMap model) {
--- src/main/java/oit/is/z1565/kaizi/janken/model/Entry.java
+++ src/main/java/oit/is/z1565/kaizi/janken/model/Entry.java
@@ -1,5 +1,35 @@
 package oit.is.z1565.kaizi.janken.model;
 
+import java.util.ArrayList;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Entry {
-  
+  ArrayList<String> users = new ArrayList<>();
+  int roomNo = 1;
+
+  public void addUser(String name) {
+    for (String s : this.users) {
+      if (s.equals(name)) {
+        return;
+      }
+    }
+    this.users.add(name);
+  }
+
+  public int getRoomNo() {
+    return roomNo;
+  }
+
+  public void setRoomNo(int roomNo) {
+    this.roomNo = roomNo;
+  }
+
+  public ArrayList<String> getUsers() {
+    return users;
+  }
+
+  public void setUsers(ArrayList<String> users) {
+    this.users = users;
+  }
 }
