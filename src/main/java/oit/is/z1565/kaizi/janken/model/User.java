8744ba218f8b11a8e07561c37a1e7f169ea51afd
YutoMina <e1b20096@st.oit.ac.jp>
2022-10-24 13:55:32 +0900

    selectを用いた情報の表示

---
+++ src/main/java/oit/is/z1565/kaizi/janken/model/User.java
@@ -0,0 +1,23 @@
+package oit.is.z1565.kaizi.janken.model;
+
+public class User {
+  int id;
+  String userName;
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUserName() {
+    return this.userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+}
